10d3149bcdcfe9f7af006acd609d687d91fea7b7
JesseCodes <88518880+Jesse3421@users.noreply.github.com>
2022-01-10 09:18:24 -0600

    deployed to heroku

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,54 +3,76 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-    "./index.html",
-    "./css/styles.css",
-    "./js/index.js", //not sure if this should be here
-    "./js/idb.js", //not sure
+    "/",
+    "/index.html",
+    "/css/styles.css",
+    "/js/index.js", 
+    "/js/idb.js", 
     
 ];
 
-// Respond with cached resources
-self.addEventListener('fetch', function (e) {
-    console.log('fetch request : ' + e.request.url)
-    e.respondWith(
-      caches.match(e.request).then(function (request) {
-        if (request) { // if cache is available, respond with cache
-          console.log('responding with cache : ' + e.request.url)
-          return request
-        } else {       // if there are no cache, try fetching request
-          console.log('file is not cached, fetching : ' + e.request.url)
-          return fetch(e.request)
-        }
-      })
-    )
-  });
-
 //Cache Resources
-self.addEventListener('install', function (e) {
-    e.waitUntil(
+self.addEventListener('install', function (evt) {
+    evt.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
             console.log('installing cache : ' + CACHE_NAME)
             return cache.addAll(FILES_TO_CACHE)
         })
     )
-    
+    self.skipWaiting();
 });
 
-// Delete cache
-self.addEventListener('activate', function (e) {
-    e.waitUntil(
-        caches.keys().then(function (keyList) {
-            let cacheKeeplist = keyList.filter(function (key) {
-                return key.indexOf(APP_PREFIX);
-            });
-            cacheKeeplist.push(CACHE_NAME);
-            return Promise.all(keyList.map(function (key, i){
-                if (cacheKeeplist.indexOf(key) === -1) {
-                    console.log('deleting cache : ' + keyList[i]);
-                    return caches.delete(keyList[i]);
-                }
+//Activate and Delete old cache
+self.addEventListener("activate", function(evt) {
+    evt.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
+    self.clients.claim();
+  });
+
+// Respond with cached resources
+self.addEventListener('fetch', function(evt) {
+    if(evt.request.url.includes('/api/')) {
+    evt.respondWith(
+        caches
+            .open(CACHE_NAME)
+            .then(cache => {
+                return fetch(evt.request)
+                    .then(response => {
+                        if(response.status === 200) {
+                            cache.put(evt.request.url, response.clone());
+                        }
+                        return response;
+                    })
+                    .catch(err => {
+                        return cache.match(evt.request);
+                    });
             })
+                .catch(err =>  console.log(err))
             )
-        }))
-});
+            return;
+            }
+
+    evt.respondWith(
+            fetch(evt.request).catch(function() {
+                return caches.match(evt.request).then(function(response) {
+                    if(response) {
+                        return response;
+                    } else if (evt.request.headers.get('accept').includes('text/html')){
+                        return caches.match('/');
+                    }
+                });
+            })
+        )
+        });
+    
